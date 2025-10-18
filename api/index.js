7adc8fe1a8975a63072fec88ad391d216eac3b39
Mudassir <mudassirpak47#gmail.com>
2025-10-18 22:34:54 +0500

    refactor: move main entry point to api/index.js and update start script

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ const path = require('path')
 const app = express()
 
 require('dotenv').config();
-const connect_DB = require('./utils/connectToMongoDB.js')
+const connect_DB = require('../utils/connectToMongoDB.js')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
-const userRouter = require('./routers/user.router.js')
+const userRouter = require('../routers/user.router.js')
 
 connect_DB();
 
 app.use("/api/v1/user", userRouter)
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is running on port ${process.env.PORT}`)
+// })
